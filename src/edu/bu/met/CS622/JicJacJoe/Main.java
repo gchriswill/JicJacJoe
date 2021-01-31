a31b8f0527f6bb542a0c428a78a3815fa26a50bb
gchriswill <4576678+gchriswill@users.noreply.github.com>
2021-01-31 05:51:42 -0600

    Updates Main class with code comments

--- src/edu/bu/met/CS622/JicJacJoe/Main.java
+++ src/edu/bu/met/CS622/JicJacJoe/Main.java
@@ -3,9 +3,15 @@ package edu.bu.met.CS622.JicJacJoe;
 import edu.bu.met.CS622.JicJacJoe.Board.BoardManager;
 
 
+/**
+ * The default class for the entry point of execution of the program
+ */
 public class Main {
 
+    // The entry point of of execution the program
     public static void main(String[] args) {
+
+        // The entry point of the Jic Jac Joe game
         BoardManager.start();
     }
 }
