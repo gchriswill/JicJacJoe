cb78103fac703ba0e85064a056800a97aa7fb43b
gchriswill <4576678+gchriswill@users.noreply.github.com>
2021-01-31 05:57:11 -0600

    Updates IllegalUserInputException class with code comments

--- src/edu/bu/met/CS622/JicJacJoe/Resources/IllegalUserInputException.java
+++ src/edu/bu/met/CS622/JicJacJoe/Resources/IllegalUserInputException.java
@@ -1,7 +1,13 @@
 package edu.bu.met.CS622.JicJacJoe.Resources;
 
-public class IllegalUserInputException extends Exception {
+/**
+ * A custom exception class type for throwing an exception when the user inputs is invalid.
+ * Uses Inheritance and Polymorphism
+ */
+public class IllegalUserInputException extends Exception { // Inheritance
+
+    // The constructor of the class.
     public IllegalUserInputException(String message) {
-        super(message);
+        super(message); // Polymorphism
     }
 }
